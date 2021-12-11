894335e0797494a608879ff111117b83b4147f20
Kevin <git@KevinFranke.nl>
2021-12-11 11:30:42 +0100

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { AppProps } from 'next/app';
 
 import '../styles/tailwind.css';
 import '../styles/main.scss';
 
 // eslint-disable-next-line react/jsx-props-no-spreading
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      // @ts-ignore gtag doesn't exsist
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+}
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,27 @@ import React from 'react';
 
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+const GA = 'G-P9ZZ3YFQSF';
+
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html lang="nl">
+        <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body className={'bg-gray-100 text-white '}>
           <Main />
           <NextScript />
