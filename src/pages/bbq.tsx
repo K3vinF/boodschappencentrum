aeae93dfb236826b9e6c7f7d325cd9e5a0e921fc
K3vinF <github@kevinfranke.nl>
2024-04-13 15:39:05 +0200

    Update bbq.tsx

--- src/pages/bbq.tsx
+++ src/pages/bbq.tsx
@@ -8,7 +8,7 @@ import BbqForm from '../Form/BbqForm';
 const Page = () => (
   <Main meta={<Meta title="Aanmelden Brunch" description="" />}>
     <Content>
-      <h1>Aanmelden BBQ 17 Juni</h1>
+      <h1>Aanmelden BBQ 29 Juni</h1>
       <p>Ben jij erbij? Meld ja aan via onderstaand formulier</p>
       <BbqForm className={'pt-6'} />
     </Content>
